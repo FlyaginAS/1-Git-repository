fafceb80ed450fc463eaebef5cac234cf7d23156
Alexander Flyagin <rus.reg.36@gmail.com>
2019-01-29 13:05:21 +0300

    28.01.19

--- trenya/main.js
+++ trenya/main.js
@@ -1,10 +1,27 @@
 'use strict';
 
+function CoffeeMachine(power, capacity) {
+    let waterAmount = 0;
 
-let id=null;
-function debounce(f, ms) {
-    return function (...args) {
-        clearTimeout(id);
-        id = setTimeout(f.bind(this, ...args), ms);
-    }
+    this.waterAmount = function(amount) {
+        // вызов без параметра, значит режим геттера, возвращаем свойство
+        if (!arguments.length) return waterAmount;
+
+        // иначе режим сеттера
+        if (amount < 0) {
+            throw new Error("Значение должно быть положительным");
+        }
+        if (amount > capacity) {
+            throw new Error("Нельзя залить воды больше, чем " + capacity);
+        }
+
+        waterAmount = amount;
+    };
+
 }
+
+let coffeeMachine = new CoffeeMachine(1000, 500);
+
+// пример использования
+coffeeMachine.waterAmount(450);
+alert( coffeeMachine.waterAmount() ); // 450
