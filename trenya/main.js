bcf2cb220c9045b3f6e8ac6499d1b2ac97c946f6
Alexander Flyagin <rus.reg.36@gmail.com>
2019-02-13 08:04:18 +0300

    13.02.2019

--- trenya/main.js
+++ trenya/main.js
@@ -1,34 +1,3 @@
 'use strict';
-class User {
-    constructor(name, surname){
-        this.name=name;
-        this.surname=surname;
-    }
-    say(){
-        console.log(`My name is ${this.name}`);
-    }
-    get fullName (){
-        return `${this.name} ${this.surname}`;
-    }
-    set fullName(full) {
-        [this.name, this.surname]=full.split(' ');
-    }
 
-}
-let admin= new User('Vasya');
-admin.say();
-console.log(admin.fullName);
-admin.fullName='Cat Catter';
-console.log(admin.fullName);
-class Rabbit extends User {
-    constructor() {
-       super();
-    }
-    say(){
-        super.say();
-        console.log('its rabbit');
-    }
-}
-let reb= new Rabbit('rab', 'rabber');
-reb.say();
 
