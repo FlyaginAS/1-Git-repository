396e543997f62fa6b6bb3147acea6ae3d66d415a
ruscat <rus.reg.36@gmail.com>
2019-03-03 18:28:51 +0300

    03.03.19

--- 10 Современные возможности ES-2015/10-11/10-11-2/main.js
+++ 10 Современные возможности ES-2015/10-11/10-11-2/main.js
@@ -2,4 +2,19 @@
 /*
 
 */
+class Animal {
+    constructor(name) {
+        this.name=name;
+    }
+    walk(){
+        console.log(`i walk ${this.name}`);
+    }
+}
+class Rabbit extends Animal {
+    walk() {
+        super.walk();
+        alert("...and jump!");
+    }
+}
 
+new Rabbit("Вася").walk();
--- 2 Основы JavaScript/main.js
+++ 2 Основы JavaScript/main.js
@@ -2,3 +2,6 @@
 /*
 
 */
+let user = '{ "name": "Вася", "age": 35, "isAdmin": false, "friends": [0,1,2,3] }';
+let arr = JSON.parse(user);
+console.log(arr);
